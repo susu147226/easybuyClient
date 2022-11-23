99bc57cf5f22a22704e58e1059be3cb58edb2d35
清婉苏苏 <2361229445@qq.com>
2022-11-23 14:03:50 +0800

    部分页面更新

--- src/view/Login.vue
+++ src/view/Login.vue
@@ -1,27 +1,27 @@
 <template>
     <page-login>
         <page-view>
-            <div class="flex flex-1 justify-center items-center mt-[8%]">
-                <div class="w-[500px] h-[450px] border border-solid ">
+            <div class="flex flex-1 justify-center items-center mt-[10%]">
+                <div class="w-[500px] border border-solid ">
                     <div class="m-[20px]">
                         <div class="flex justify-between items-center mt-[50px]">
                             <h2 class="h2">欢迎登录</h2>
                             <el-link type="primary">去注册</el-link>
                         </div>
-                        <el-form class="mt-[40px]">
+                        <el-form class="mt-[40px] mb-[20px]">
                             <el-form-item>
-                                <el-input placeholder="邮箱/手机号"></el-input>
+                                <el-input size="large" placeholder="邮箱/手机号"></el-input>
                             </el-form-item>
                             <el-form-item>
-                                <el-input placeholder="密码"></el-input>
+                                <el-input size="large" placeholder="密码"></el-input>
                             </el-form-item>
                             <el-form-item>
-                                <el-checkbox-group>
-                                    <el-checkbox label="已同意并阅读小米账号 用户协议 和 隐私政策" name="type" />
-                                </el-checkbox-group>
+                                <el-checkbox :v-model="!isagree" label="已同意并阅读小米账号 用户协议 和 隐私政策" name="type" />
                             </el-form-item>
                             <el-form-item>
-                                <el-button class="w-full" type='warning'>登录系统</el-button>
+                                <el-button class="w-full" size="large" color="#ff8a00">
+                                    <span class="text-white font-bold">登录系统</span>
+                                </el-button>
                             </el-form-item>
                         </el-form>
                     </div>
@@ -32,7 +32,9 @@
 
 </template>
 <script setup>
+import { ref } from "vue";
 
+const isagree = ref(false);
 </script>
 <style scoped lang="scss">
 .h2 {
