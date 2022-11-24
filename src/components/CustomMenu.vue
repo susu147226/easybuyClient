3f7b55ce91c629d8da8638ff06a3a3c1fc29908b
清婉苏苏 <2361229445@qq.com>
2022-11-24 09:22:31 +0800

    用户主页

---
+++ src/components/CustomMenu.vue
@@ -0,0 +1,15 @@
+<template>
+    <page-view>
+        <title-bar></title-bar>
+        <div class="custom-menu">
+            
+        </div>
+        <foot-bar></foot-bar>
+    </page-view>
+</template>
+<script setup>
+
+</script>
+<style scoped lang="scss">
+
+</style>
---
+++ src/views/customInfo/CustomInfo.vue
@@ -0,0 +1,11 @@
+<template>
+    <page-view>
+        
+    </page-view>
+</template>
+<script setup>
+
+</script>
+<style scoped lang="scss">
+
+</style>
